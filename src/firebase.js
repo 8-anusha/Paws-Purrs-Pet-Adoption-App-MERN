16c9e8486be2b9883a817456e399640221270573
8-anusha <156271857+8-anusha@users.noreply.github.com>
2024-07-11 17:41:11 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,36 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: 'mern-estate.firebaseapp.com',
+//   projectId: 'mern-estate',
+//   storageBucket: 'mern-estate.appspot.com',
+//   messagingSenderId: '1078482850952',
+//   appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+// Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA5z-hky_0l0n8O8gG0F0a_a2P0txfraVA",
+  authDomain: "mern-estate-6fca1.firebaseapp.com",
+  projectId: "mern-estate-6fca1",
+  storageBucket: "mern-estate-6fca1.appspot.com",
+  messagingSenderId: "827562995028",
+  appId: "1:827562995028:web:18530b015369f21c0cecc1"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+//import React from 'react';
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-pink-800'>About paws &purrs</h1>
+      <p className='mb-4 text-pink-700'>Paws and Purr is a dedicated pet adoption agency that specializes in helping clients find their perfect furry companion. Our team of experienced staff is committed to providing exceptional service and making the pet adoption process as smooth as possible</p>
+      <p className='mb-4 text-pink-700'>
+      Our mission is to help our clients achieve their pet adoption goals by providing expert advice, personalized service, and a deep understanding of the local pet community. Whether you are looking to adopt a pet, we are here to help you every step of the way.
+      </p>
+      <p className='mb-4 text-pink-700'>Our team of staff has a wealth of experience and knowledge in the pet adoption industry, and we are committed to providing the highest level of service to our clients. We believe that adopting a pet should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
+    </div>
+  )
+}
